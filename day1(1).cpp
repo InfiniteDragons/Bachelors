975465866b648e798b3e184a9b71d7c698f9a32b
Dragon <166578593+InfiniteDragons@users.noreply.github.com>
2025-05-14 19:00:37 +0545

    Add files via upload

---
+++ day1(1).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+ using namespace std;
+
+ void sum(int x,int y)
+ {
+    int sum;
+    sum = x + y;
+    cout<<"Sum of "<<x<<" & "<<y<<" is "<<sum;
+ }
+
+ int main()
+ {
+    int a, b;
+    cout<<"Enter two numbers to be added"<<endl;
+    std::cin>>a>>b;
+    sum(a,b);
+    return 0;
+ }
